b603ba346aae760bbaac2ef7f2ce95da1b86ffa1
이지호 <cctv1998@naver.com>
2022-02-07 22:51:08 +0900

    Feat : initialized 설정
    Co-authored-by: 이도현 <ksmfou98@users.noreply.github.com>

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'airbnb',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'arrow-body-style': 'off',
+    'react/prop-types': 'off',
+    'react/require-default-props': 'off',
+    'react/jsx-filename-extension': [
+      'error',
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    'react/no-array-index-key': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'jsx-a11y/no-noninteractive-element-interactions': 'off',
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': 'error',
+  },
+};
---
+++ src/components/Asd.tsx
@@ -0,0 +1,10 @@
+import React, { ReactElement } from 'react';
+
+function Asd(): ReactElement {
+  const onClick = () => {
+    const asd: any = 1;
+  };
+  return <div>안녕하세요</div>;
+}
+
+export default Asd;
