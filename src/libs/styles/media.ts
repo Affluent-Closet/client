b20c6c17ecf42bcc4febc7e8e89b9930bb576355
이지호 <cctv1998@naver.com>
2022-02-10 21:44:18 +0900

    Feat : media

---
+++ src/libs/styles/media.ts
@@ -0,0 +1,32 @@
+export const mediaQuery = (maxWidth: number) =>
+  `@media (max-width: ${maxWidth}px)`;
+
+/**
+ * xxlarge : large Desktop size,
+ * xlarge : Desktop size,
+ * large : large Tablet size,
+ * medium : Tabket size,
+ * small : large Mobile size,
+ * xsmall : Mobile size,
+ */
+export const mediaSize = {
+  xxlarge: 1920,
+  xlarge: 1366,
+  large: 1024,
+  medium: 768,
+  small: 414,
+  xsmall: 320,
+};
+
+const { xxlarge, xlarge, large, medium, small, xsmall } = mediaSize;
+
+const media = {
+  xxlarge: mediaQuery(xxlarge),
+  xlarge: mediaQuery(xlarge),
+  large: mediaQuery(large),
+  medium: mediaQuery(medium),
+  small: mediaQuery(small),
+  xsmall: mediaQuery(xsmall),
+};
+
+export default media;
