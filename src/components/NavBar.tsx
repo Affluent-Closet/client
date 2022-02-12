544840c70f3da3b8793e2eac7f05e0f4e9fbc890
이지호 <cctv1998@naver.com>
2022-02-12 23:29:47 +0900

    Styel : nanbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
-import Path from 'routes/Path';
 import { palette } from 'libs/styles/palette';
 import media from 'libs/styles/media';
+import { categoryMenus } from 'libs/categoryMenus';
+import { communityMenus } from 'libs/communityMenus';
 
 const NavBarBlock = styled.div`
-  position: absolute;
+  position: fixed;
   background-color: ${palette.backgroundColor};
-  width: 50%;
+  width: 75%;
   max-width: 331px;
   height: 100%;
   left: 0px;
@@ -16,7 +17,7 @@ const NavBarBlock = styled.div`
 `;
 
 const NavBarH2 = styled.h2`
-  margin-left: 20px;
+  margin: 25px 30px;
   font-weight: normal;
   font-size: 24px;
   line-height: 28px;
@@ -27,103 +28,56 @@ const NavBarH2 = styled.h2`
   color: ${palette.mainColor};
 `;
 
-const Menus = styled.div`
-  text-decoration-line: none;
-  margin-left: 30px;
-  font-style: normal;
-  font-weight: normal;
+const Infos = styled.div`
+  margin: 6px 30px;
   font-size: 18px;
   line-height: 21px;
-  display: flex;
-  align-items: center;
-  text-align: center;
+  text-align: left;
+  width: 10rem;
+  padding: 5px 20px;
   text-transform: uppercase;
   ${media.xsmall} {
     margin-left: 15px;
   }
+  &:hover {
+    background: ${palette.backgroundHover};
+    border-radius: 8px;
+  }
 `;
 
-const activeStyle: React.CSSProperties = {
-  backgroundColor: `${palette.backgroundHover}`,
-  color: `${palette.mainLightColor}`,
-};
-
-const nonActiveStyle: React.CSSProperties = {
-  color: 'black',
-};
+const CommunityMenuListStyled = styled.div`
+  .active {
+    .name {
+      background: ${palette.backgroundHover};
+      border-radius: 8px;
+    }
+  }
+`;
 
 function NavBar() {
-  const categoryMenus = [
-    {
-      name: 'Best',
-      href: `${Path.TestPage}`,
-    },
-    {
-      name: 'New Arrival',
-      href: `${Path.HomePage}`,
-    },
-    {
-      name: 'Outer',
-      href: '/Shirt',
-    },
-    {
-      name: 'Shirt',
-      href: '/Shirt',
-    },
-    {
-      name: 'Top',
-      href: '/Top',
-    },
-    {
-      name: 'Knit',
-      href: '/Knit',
-    },
-    {
-      name: 'Bottom',
-      href: '/Bottom',
-    },
-    {
-      name: 'Acc',
-      href: '/Acc',
-    },
-  ];
-
-  const communityMenus = [
-    {
-      name: 'Login',
-      href: '/login',
-    },
-    {
-      name: 'Mypage',
-      href: '/mypage',
-    },
-    {
-      name: 'Like',
-      href: '/like',
-    },
-  ];
   return (
     <NavBarBlock>
-      <div className="category-menu">
+      <CommunityMenuListStyled>
         <NavBarH2>CATEGORY</NavBarH2>
-        {categoryMenus.map((categoryMenu, index) => (
-          <NavLink to={categoryMenu.href} key={index}>
-            <Menus className="name">{categoryMenu.name}</Menus>
+        {categoryMenus.map(({ href, name }, index) => (
+          <NavLink to={href} key={`CategoryMenu_${index}`}>
+            <Infos className="name">{name}</Infos>
           </NavLink>
         ))}
-      </div>
-      <div className="community-menu">
+      </CommunityMenuListStyled>
+
+      <CommunityMenuListStyled>
         <NavBarH2>CATEGORY</NavBarH2>
-        {communityMenus.map((communityMenu, index) => (
+        {communityMenus.map(({ href, name }) => (
           <NavLink
-            to={communityMenu.href}
-            key={index}
-            style={({ isActive }) => (isActive ? activeStyle : nonActiveStyle)}
+            to={href}
+            key={`CommunityMenu_${name}`}
+            className={({ isActive }) => (isActive ? 'active' : '')}
           >
-            <Menus className="name">{communityMenu.name}</Menus>
+            <Infos className="name">{name}</Infos>
           </NavLink>
         ))}
-      </div>
+      </CommunityMenuListStyled>
     </NavBarBlock>
   );
 }
