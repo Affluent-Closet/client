5b64f296df809e7c2198f38d168d41f7fa7dd38c
이지호 <cctv1998@naver.com>
2022-02-12 23:29:02 +0900

    Feat : App 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
+import Header from 'components/Header';
 import NavBar from 'components/NavBar';
 import GlobalStyles from 'libs/styles/globalStyles';
 import React from 'react';
+import styled from 'styled-components';
 import './App.css';
 import Routing from './routes/Routing';
+
+const AsideLayout = styled.div`
+  margin-left: 331px;
+  height: 100%;
+`;
 
 function App() {
   return (
     <div className="App">
-      <GlobalStyles />
-      <NavBar />
-      <Routing />
+      <Header />
+      <AsideLayout>
+        <GlobalStyles />
+        {/* <NavBar /> */}
+        <Routing />
+      </AsideLayout>
     </div>
   );
 }
