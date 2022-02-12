daad0e30b1fde11ef14bcdc1014c58e7aef248b3
이지호 <cctv1998@naver.com>
2022-02-12 23:29:35 +0900

    Refactor : menulist 분리

---
+++ src/libs/communityMenus.ts
@@ -0,0 +1,14 @@
+export const communityMenus = [
+  {
+    name: 'Login',
+    href: '/login',
+  },
+  {
+    name: 'Mypage',
+    href: '/mypage',
+  },
+  {
+    name: 'Like',
+    href: '/like',
+  },
+];
