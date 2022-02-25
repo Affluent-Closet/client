0aa336766b6b2eae235e9a570e6efbb401a6e74a
이지호 <cctv1998@naver.com>
2022-02-26 00:51:52 +0900

    Refactor  :TabBox 컴포넌트 분리

--- src/Pages/GoodsPage.tsx
+++ src/Pages/GoodsPage.tsx
@@ -1,6 +1,6 @@
 import GoodsInfo from 'components/Goods/GoodsInfo';
+import TabBox from 'components/Goods/TabBox';
 import useMoveScrool from 'hooks/common/useMoveScrool';
-import { palette } from 'libs/styles/palette';
 import React from 'react';
 import styled from 'styled-components';
 
@@ -14,31 +14,6 @@ const GoodsImg = styled.img`
   margin: 0 auto;
 `;
 
-const TabBox = styled.div`
-  display: flex;
-  justify-content: center;
-  background-color: ${palette.backgroundColor};
-  margin: 0px 30px;
-  .active {
-    color: black;
-    font-weight: 500;
-    background-color: white;
-  }
-`;
-
-const TabStyled = styled.button`
-  display: block;
-  text-align: center;
-  height: 60px;
-  box-sizing: border-box;
-  font-size: 16px;
-  line-height: 22px;
-  color: #a0a0a0;
-  border: solid 1px ${palette.backgroundHover};
-  padding: 20px 0;
-  width: 34%;
-`;
-
 function GoodsPage() {
   const goodsTabs = {
     0: useMoveScrool('상품 상세'),
@@ -49,51 +24,15 @@ function GoodsPage() {
   return (
     <>
       <GoodsInfo />
-      <TabBox ref={goodsTabs[0].element}>
-        {Array.from(goodsTabs).map(({ onMoveToElement, name }, index) => (
-          <TabStyled
-            type="button"
-            onClick={onMoveToElement}
-            key={`goodsTabs_${index}`}
-            className={index === 0 ? 'active' : ''}
-          >
-            {name}
-          </TabStyled>
-        ))}
-      </TabBox>
+      <TabBox goodsTabs={Array.from(goodsTabs)} th={0} />
+
       <GoodsDetailWrapper>
         <GoodsImg
-          src="https://image.msscdn.net/images/prd_img/20220121/2319884/detail_2319884_2_500.jpg"
+          src="https://intempomood.cafe24.com/2022/2022SS/Full Zip-up Cardigan_Black_01.jpg"
           alt="의상 이미지"
         />
-        <GoodsImg
-          src="https://image.msscdn.net/images/prd_img/20220121/2319884/detail_2319884_2_500.jpg"
-          alt="의상 이미지"
-        />
-        <TabBox ref={goodsTabs[1].element}>
-          {Array.from(goodsTabs).map(({ onMoveToElement, name }, index) => (
-            <TabStyled
-              type="button"
-              onClick={onMoveToElement}
-              key={`goodsTabs_${index}`}
-              className={index === 1 ? 'active' : ''}
-            >
-              {name}
-            </TabStyled>
-          ))}
-        </TabBox>
-        <TabBox ref={goodsTabs[1].element}>
-          {Array.from(goodsTabs).map(({ onMoveToElement, name }, index) => (
-            <TabStyled
-              type="button"
-              onClick={onMoveToElement}
-              key={`goodsTabs_${index}`}
-              className={index === 2 ? 'active' : ''}
-            >
-              {name}
-            </TabStyled>
-          ))}
-        </TabBox>
+        <TabBox goodsTabs={Array.from(goodsTabs)} th={1} />
+        <TabBox goodsTabs={Array.from(goodsTabs)} th={2} />
       </GoodsDetailWrapper>
     </>
   );
