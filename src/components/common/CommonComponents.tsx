ab81ca9deacee40deec44749d51d6635f06b70fc
이지호 <cctv1998@naver.com>
2022-02-26 16:27:53 +0900

    Feat : CommonCompoenents

---
+++ src/components/common/CommonComponents.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const FlexCenter = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const FlexBetween = styled.div`
+  display: flex;
+  justify-content: center;
+`;
