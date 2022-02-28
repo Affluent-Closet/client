a894072db83d0e2766b0d093ac5b7eedd242c990
이지호 <cctv1998@naver.com>
2022-02-28 23:50:08 +0900

    Feat : useSlider

---
+++ src/hooks/common/useSlider.ts
@@ -0,0 +1,31 @@
+import { useEffect, useRef, useState } from 'react';
+
+function useSlider() {
+  const TOTAL_SLIDES = 2;
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const element = useRef<HTMLDivElement>(null);
+  const nextSlide = () => {
+    if (currentSlide >= TOTAL_SLIDES) {
+      // 더 이상 넘어갈 슬라이드가 없으면 슬라이드를 초기화합니다.
+      setCurrentSlide(0);
+    } else {
+      setCurrentSlide(currentSlide + 1);
+    }
+  };
+  const prevSlide = () => {
+    if (currentSlide === 0) {
+      setCurrentSlide(TOTAL_SLIDES);
+    } else {
+      setCurrentSlide(currentSlide - 1);
+    }
+  };
+  useEffect(() => {
+    if (!element.current) return;
+    element.current.style.transition = 'all 0.3s ease-in-out';
+    element.current.style.transform = `translateX(-${currentSlide}00%)`; // 백틱을 사용하여 슬라이드로 이동하는 애니메이션을 만듭니다.
+  }, [currentSlide]);
+
+  return { currentSlide, element, nextSlide, prevSlide };
+}
+
+export default useSlider;
