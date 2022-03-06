0c585768d6189656c565819b0e70a239ab4a4b9e
이지호 <cctv1998@naver.com>
2022-03-06 23:13:30 +0900

    Style : GoodsInfoWrapper 캐로셀, 구매정보

--- src/components/Goods/ReviewGoodsInfo.tsx
+++ src/components/Goods/ReviewGoodsInfo.tsx
@@ -1,17 +1,49 @@
+import { FlexBetween, FlexCenter } from 'components/common/CommonComponents';
+import { palette } from 'libs/styles/palette';
 import React from 'react';
 import styled from 'styled-components';
 
+const GoodsInfoWrapper = styled(FlexBetween)`
+  font-size: 13px;
+  align-items: center;
+  padding-right: 20px;
+  .body-info {
+    color: ${palette.temp};
+  }
+`;
+
 const Image = styled.img`
   width: 45px;
+  height: 54px;
+  border-radius: 4px;
 `;
+
+const Item = styled.div`
+  margin-left: 10px;
+  padding-top: 1px;
+  display: flex;
+  flex-direction: column;
+  .purchase {
+    margin-top: 4px;
+    color: ${palette.temp};
+  }
+`;
+
 function ReviewGoodsInfo() {
   return (
-    <div>
-      <Image
-        src="https://image.msscdn.net/images/goods_img/20220105/2292885/2292885_2_100.jpg"
-        alt="이미지"
-      />
-    </div>
+    <GoodsInfoWrapper>
+      <FlexCenter>
+        <Image
+          src="https://image.msscdn.net/images/prd_img/20220121/2319884/detail_2319884_2_500.jpg"
+          alt="이미지"
+        />
+        <Item>
+          <div>데이브레이크 운동화</div>
+          <div className="purchase">M 구매</div>
+        </Item>
+      </FlexCenter>
+      <div className="body-info">남성, 180cm, 85kg</div>
+    </GoodsInfoWrapper>
   );
 }
 
