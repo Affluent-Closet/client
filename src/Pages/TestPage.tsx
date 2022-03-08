04a0f60f528657a0696876c0861cc316b92eb15b
이지호 <cctv1998@naver.com>
2022-03-08 22:43:15 +0900

    Delete : testPage

--- src/Pages/TestPage.tsx
+++ src/Pages/TestPage.tsx
@@ -1,43 +1,7 @@
-import Slide from 'components/goods/Slide';
-import useSlider from 'hooks/common/useSlider';
 import React from 'react';
-import styled from 'styled-components';
-
-const Container = styled.div`
-  width: 60%;
-  overflow: hidden; // 선을 넘어간 이미지들은 보이지 않도록 처리합니다.
-`;
-const Button = styled.button`
-  all: unset;
-  border: 1px solid coral;
-  padding: 0.5em 2em;
-  color: coral;
-  border-radius: 10px;
-  &:hover {
-    transition: all 0.3s ease-in-out;
-    background-color: coral;
-    color: #fff;
-  }
-`;
-const SliderContainer = styled.div`
-  width: 100%;
-  display: flex; //이미지들을 가로로 나열합니다.
-`;
 
 function TestPage() {
-  const { currentSlide, element, nextSlide, prevSlide } = useSlider();
-  return (
-    <Container>
-      {currentSlide}
-      <SliderContainer ref={element}>
-        <Slide img="https://media.vlpt.us/images/peppermint100/post/d99e3dff-779f-4000-9857-5844481da5b8/image.png" />
-        <Slide img="https://media.vlpt.us/images/peppermint100/post/d99e3dff-779f-4000-9857-5844481da5b8/image.png" />
-        <Slide img="https://media.vlpt.us/images/peppermint100/post/d99e3dff-779f-4000-9857-5844481da5b8/image.png" />
-      </SliderContainer>
-      <Button onClick={prevSlide}>Previous Slide</Button>
-      <Button onClick={nextSlide}>Next Slide</Button>
-    </Container>
-  );
+  return <div>테스트</div>;
 }
 
 export default TestPage;
