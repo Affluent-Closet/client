15582629da2a6297e1591201f09763b92711fe5d
이지호 <cctv1998@naver.com>
2022-03-09 17:56:14 +0900

    Feat : ItemGrid 전역사용

--- src/components/common/CommonComponents.tsx
+++ src/components/common/CommonComponents.tsx
@@ -1,3 +1,4 @@
+import { mediaMin } from 'libs/styles/media';
 import styled from 'styled-components';
 
 export const FlexCenter = styled.div`
@@ -10,4 +11,24 @@ export const FlexBetween = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+`;
+
+export const ItemGrid = styled.div`
+  display: grid;
+  grid-template-rows: 1fr 1fr;
+  grid-template-columns: 1fr 1fr;
+
+  ${mediaMin.medium} {
+    grid-template-rows: 1fr 1fr;
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+
+  ${mediaMin.large} {
+    grid-template-rows: 1fr 1fr;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+  }
+  ${mediaMin.xlarge} {
+    grid-template-rows: 1fr 1fr;
+    grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+  }
 `;
