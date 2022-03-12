0c09f21f9dce88f25ed6c4ac70bf4e188a0642d7
이지호 <cctv1998@naver.com>
2022-03-12 20:27:54 +0900

    Test : recoil

--- src/Pages/TestPage.tsx
+++ src/Pages/TestPage.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import { charCountState, textState } from 'atoms/textState';
+import React, { ChangeEvent } from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
 
 function TestPage() {
-  return <div>테스트</div>;
+  const [text, setText] = useRecoilState(textState);
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+  const count = useRecoilValue(charCountState);
+  return (
+    <div>
+      <input type="text" value={text} onChange={onChange} />
+      <br />
+      Echo: {text}
+      count : {count}
+    </div>
+  );
 }
 
 export default TestPage;
