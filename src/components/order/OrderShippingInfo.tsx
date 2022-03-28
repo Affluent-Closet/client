1243dd170769eaf99a48bc7857b1672224870ebb
이재하 <jhl2619@naver.com>
2022-03-28 12:40:34 +0900

    feat : Add Responsive OrderShippingInfo

--- src/components/order/OrderShippingInfo.tsx
+++ src/components/order/OrderShippingInfo.tsx
@@ -1,18 +1,29 @@
 import Button from 'components/common/Button';
 import { OrderPageInner } from 'components/common/CommonComponents';
 import Input from 'components/common/Input';
+import { mediaMin } from 'libs/styles/media';
 import React from 'react';
 import styled from 'styled-components';
 
 const ShippingInfoSection = styled.section`
   margin: 20px 0;
 `;
-const ShippingInfoList = styled.ol``;
-const ShippingInfoItem = styled.li`
+const ShippingInfoItem = styled.div`
   margin-top: 8px;
+  ${mediaMin.large} {
+    display: flex;
+    flex-direction: row;
+  }
 `;
 
-const ItemText = styled.span``;
+const ShippingInfoCategory = styled.span`
+  width: 100px;
+  ${mediaMin.large} {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
 const ItemInput = styled(Input)`
   margin-top: 8px;
 `;
@@ -22,29 +33,34 @@ const SearchAddr = styled.div`
   flex-direction: row;
   align-items: baseline;
 `;
+const ShippingInfoAdrrssWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
 
 function OrderShippingInfo() {
   return (
     <ShippingInfoSection>
       <h3>배송 정보</h3>
       <OrderPageInner>
-        <ShippingInfoList>
-          <ShippingInfoItem>
-            <ItemText>
-              <h4>수령인</h4>
-            </ItemText>
-            <ItemInput width="100%" height="auto" />
-          </ShippingInfoItem>
-          <ShippingInfoItem>
-            <ItemText>
-              <h4>연락처</h4>
-            </ItemText>
-            <ItemInput width="100%" height="auto" />
-          </ShippingInfoItem>
-          <ShippingInfoItem>
-            <ItemText>
-              <h4>배송지</h4>
-            </ItemText>
+        <ShippingInfoItem>
+          <ShippingInfoCategory>
+            <h4>수령인</h4>
+          </ShippingInfoCategory>
+          <ItemInput width="100%" height="auto" />
+        </ShippingInfoItem>
+        <ShippingInfoItem>
+          <ShippingInfoCategory>
+            <h4>연락처</h4>
+          </ShippingInfoCategory>
+          <ItemInput width="100%" height="auto" />
+        </ShippingInfoItem>
+        <ShippingInfoItem>
+          <ShippingInfoCategory>
+            <h4>배송지</h4>
+          </ShippingInfoCategory>
+          <ShippingInfoAdrrssWrapper>
             <SearchAddr>
               <ItemInput width="100%" height="auto" />
               <Button width="80px" height="30px">
@@ -53,14 +69,14 @@ function OrderShippingInfo() {
             </SearchAddr>
             <ItemInput width="100%" height="auto" />
             <ItemInput width="100%" height="auto" />
-          </ShippingInfoItem>
-          <ShippingInfoItem>
-            <ItemText>
-              <h4>요청사항</h4>
-            </ItemText>
-            <ItemInput width="100%" height="auto" />
-          </ShippingInfoItem>
-        </ShippingInfoList>
+          </ShippingInfoAdrrssWrapper>
+        </ShippingInfoItem>
+        <ShippingInfoItem>
+          <ShippingInfoCategory>
+            <h4>요청사항</h4>
+          </ShippingInfoCategory>
+          <ItemInput width="100%" height="auto" />
+        </ShippingInfoItem>
       </OrderPageInner>
     </ShippingInfoSection>
   );
