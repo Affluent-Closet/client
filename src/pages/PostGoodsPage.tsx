ade1e653f340e82162e0c95fa9a0ffc3450133cf
이지호 <cctv1998@naver.com>
2022-03-29 22:24:20 +0900

    Feat : PostGoodsPage

--- src/pages/PostGoodsPage.tsx
+++ src/pages/PostGoodsPage.tsx
@@ -1,7 +1,15 @@
+import { PageContainer } from 'components/common/CommonComponents';
+import PostGoodsBox from 'components/goodspost/PostGoodsBox';
+import PostGoodsHead from 'components/goodspost/PostGoodsHead';
 import React from 'react';
 
 function PostGoodsPage() {
-  return <div>PostGoodsPage</div>;
+  return (
+    <PageContainer>
+      <PostGoodsHead />
+      <PostGoodsBox />
+    </PageContainer>
+  );
 }
 
 export default PostGoodsPage;
