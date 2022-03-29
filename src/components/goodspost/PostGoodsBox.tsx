a48d72b324f207cf8648751204d3f458b0fa2a00
이지호 <cctv1998@naver.com>
2022-03-29 23:14:59 +0900

    Style : PostGoodsBox 스타일 보충

--- src/components/goodspost/PostGoodsBox.tsx
+++ src/components/goodspost/PostGoodsBox.tsx
@@ -1,6 +1,8 @@
+import Button from 'components/common/Button';
 import {
   FlexAlignCenter,
   InputStyled,
+  ItemGrid,
 } from 'components/common/CommonComponents';
 import { categoryMenus } from 'libs/Lists/NavItems';
 import { mediaMax } from 'libs/styles/media';
@@ -24,24 +26,58 @@ const PostInputBox = styled(FlexAlignCenter)`
 `;
 
 const PostAns = styled(InputStyled)`
-  height: 30px;
+  height: 25px;
   width: 50%;
   margin-right: 10px;
+  font-size: 12px;
+  ${mediaMax.medium} {
+    width: 80%;
+  }
+  ${mediaMax.small} {
+    width: 100%;
+  }
 `;
 
 const SelectedStyled = styled.select`
   width: 50%;
-  height: 30px;
+  height: 25px;
   border: 1px solid ${palette.border};
+  border-radius: 4px;
+  padding-left: 10px;
+  font-size: 12px;
+  ${mediaMax.medium} {
+    width: 80%;
+  }
+  ${mediaMax.small} {
+    width: 100%;
+  }
+`;
+const BtnBox = styled.div`
+  text-align: right;
+  margin: 15px;
+`;
+
+const ImgBox = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: antiquewhite;
+  margin: 10px;
 `;
 function PostGoodsBox() {
   return (
     <>
       <PostInputBox>
         <PostQue>썸네일</PostQue>
+        <ImgBox />
       </PostInputBox>
       <PostInputBox>
         <PostQue>사진</PostQue>
+        <ItemGrid>
+          <ImgBox /> <ImgBox />
+          <ImgBox /> <ImgBox />
+          <ImgBox /> <ImgBox />
+          <ImgBox /> <ImgBox />
+        </ItemGrid>
       </PostInputBox>
       <PostInputBox>
         <PostQue>제목</PostQue>
@@ -63,11 +99,25 @@ function PostGoodsBox() {
         <p>원</p>
       </PostInputBox>
       <PostInputBox>
-        <PostQue>사진</PostQue>
+        <PostQue>할인률</PostQue>
+        <PostAns type="number" placeholder="할인률(%)" />
+        <p>%</p>
       </PostInputBox>
       <PostInputBox>
         <PostQue>재고</PostQue>
+        <PostAns type="number" placeholder="갯수" />
       </PostInputBox>
+      <BtnBox>
+        <Button
+          width="100px"
+          height="30px"
+          buttonColor="mainColor"
+          fontColor="white"
+          hoverButtonColor="mainHoverColor"
+        >
+          등록하기
+        </Button>
+      </BtnBox>
     </>
   );
 }
