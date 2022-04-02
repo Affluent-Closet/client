4a20608c9e1734572d13d1d8bc7272c221c0ad40
이지호 <cctv1998@naver.com>
2022-04-03 01:48:48 +0900

    Refactor : 중복되는 코드 제거

--- src/components/reviewpost/PostReviewBox.tsx
+++ src/components/reviewpost/PostReviewBox.tsx
@@ -2,51 +2,25 @@ import Button from 'components/common/Button';
 import {
   FlexAlignCenter,
   FlexCenter,
-  InputStyled,
   ItemGrid,
 } from 'components/common/CommonComponents';
 import { ReviewImg } from 'components/goods/ReviewItem';
+import {
+  BtnBox,
+  ImgBox,
+  InputFileLabel,
+  InputFileStyled,
+  PostAns,
+  PostQue,
+} from 'components/goodspost/PostGoodsBox';
 import { mediaMax } from 'libs/styles/media';
 import { palette } from 'libs/styles/palette';
-import React from 'react';
-import { AiFillStar } from 'react-icons/ai';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-
-const StarLabel = styled.label`
-  color: ${palette.border};
-`;
-const StarButton = styled.input`
-  display: none;
-  &:checked + ${StarLabel} {
-    color: coral;
-  }
-`;
-const PostQue = styled(FlexAlignCenter)`
-  width: 120px;
-  font-size: 17px;
-  height: 50px;
-  margin: 10px;
-  ${mediaMax.medium} {
-    width: 60px;
-    font-size: 14px;
-  }
-`;
+import RatingSection from './RatingSection';
 
 const PostInputBox = styled(FlexAlignCenter)`
   border-bottom: 1px solid ${palette.border};
-`;
-
-const PostAns = styled(InputStyled)`
-  height: 25px;
-  width: 50%;
-  margin-right: 10px;
-  font-size: 12px;
-  ${mediaMax.medium} {
-    width: 80%;
-  }
-  ${mediaMax.small} {
-    width: 100%;
-  }
 `;
 
 const PostDetailAns = styled.textarea`
@@ -62,37 +36,6 @@ const PostDetailAns = styled.textarea`
   }
 `;
 
-const BtnBox = styled.div`
-  text-align: right;
-  margin: 15px;
-`;
-
-const ImgBox = styled.div`
-  width: 100px;
-  height: 100px;
-  background-color: antiquewhite;
-  margin: 10px;
-`;
-
-const InputFileStyled = styled.input`
-  display: none;
-`;
-
-const InputFileLabel = styled.label`
-  display: inline-block;
-  padding: 10px 20px;
-  color: #fff;
-  vertical-align: middle;
-  background-color: #999999;
-  cursor: pointer;
-  height: 40px;
-  margin-left: 10px;
-`;
-
-const RatingSection = styled(FlexAlignCenter)`
-  margin: 13px 0px;
-`;
-
 const ReviewGoodsImg = styled(ReviewImg)`
   margin: 15px 0;
 `;
@@ -104,7 +47,7 @@ const ReviewGoodsTit = styled.div`
   font-weight: 500;
 `;
 function PostReviewBox() {
-  const ratingIndexes = [1, 2, 3, 4, 5];
+  const [ratingIndex, setRatingIndex] = useState(1);
   return (
     <>
       <PostInputBox>
@@ -116,16 +59,10 @@ function PostReviewBox() {
       </PostInputBox>
       <PostInputBox>
         <PostQue>평점</PostQue>
-        <RatingSection>
-          {ratingIndexes.map((rating, index) => (
-            <div className={`rating_${index}`}>
-              <StarButton type="checkbox" id={`r${rating}`} name="rating" />
-              <StarLabel htmlFor={`r${rating}`}>
-                <AiFillStar size={35} />
-              </StarLabel>
-            </div>
-          ))}
-        </RatingSection>
+        <RatingSection
+          ratingIndex={ratingIndex}
+          setRatingIndex={setRatingIndex}
+        />
       </PostInputBox>
       <PostInputBox>
         <div>
