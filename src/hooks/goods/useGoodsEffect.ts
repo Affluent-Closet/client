818d5a4d58101f6280692fde0f676886a20b25de
이지호 <cctv1998@naver.com>
2022-04-06 04:25:56 +0900

    Refactor : useGoodsEffect 구조 변경

--- src/hooks/goods/useGoodsEffect.ts
+++ src/hooks/goods/useGoodsEffect.ts
@@ -1,19 +1,17 @@
-import useGoodsQueryForm from 'hooks/search/useGoodsQueryForm';
 import { getGoodsAPI } from 'libs/api/goodsAPI';
-import { IGoodsResponse } from 'model/goods';
+import { IGoodsQuery, IGoodsResponse } from 'model/goods';
 import { useQuery } from 'react-query';
 
-function useGoodsEffect() {
-  const { goodsQueryString, onChangeGoodsQueryString } = useGoodsQueryForm();
-  const { pageNo } = goodsQueryString;
+function useGoodsEffect(goodsQueryString: IGoodsQuery) {
   const goodsData = useQuery<IGoodsResponse>(
-    ['goods', pageNo],
+    ['goods', goodsQueryString.pageNo, goodsQueryString.sortBy],
     () => getGoodsAPI(goodsQueryString),
     {
       keepPreviousData: true,
     },
   );
-  return { goodsData, onChangeGoodsQueryString };
+
+  return { goodsData };
 }
 
 export default useGoodsEffect;
