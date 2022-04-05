84a7aa9c5f484882e3233b0392554e2655be260d
이지호 <cctv1998@naver.com>
2022-04-05 23:06:37 +0900

    Feat : getGoodsAPI query 받을 수 있게 변경

--- src/libs/api/goodsAPI.ts
+++ src/libs/api/goodsAPI.ts
@@ -1,6 +1,14 @@
 import client from '.';
 
-export const getGoodsAPI = async (pageNo: number) => {
-  const response = await client.get(`/goods?pageNo=${pageNo}`);
+export const getGoodsAPI = async (
+  pageNo: number,
+  pageSize?: number,
+  sortBy?: string,
+  name?: string,
+  category?: string,
+) => {
+  const response = await client.get(
+    `/goods?pageNo=${pageNo}&pageSize=${pageSize}&sortBy=${sortBy}&name=${name}&category=${category}`,
+  );
   return response.data;
 };
