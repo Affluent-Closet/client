3ec27f02dca9c0af7a8839f3114aad92f0a30662
이지호 <cctv1998@naver.com>
2022-04-11 00:44:28 +0900

    Feat : useGoodsQueryForm 수정된거에 맞게 변경

--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -2,22 +2,15 @@ import { PageContainer } from 'components/common/CommonComponents';
 import { SearchTit } from 'components/search/SearchField';
 import ResultItems from 'components/search/ResultItems';
 import React from 'react';
-import { useParams } from 'react-router-dom';
 import useGoodsQueryForm from 'hooks/search/useGoodsQueryForm';
 
 function CategoryPage() {
-  const params = useParams();
-  const { goodsQueryString, onClickGoodsQS } = useGoodsQueryForm(
-    undefined,
-    undefined,
-    `${params.categoryid?.toUpperCase()}`,
-  );
+  const { goodsQueryString, onClickGoodsQS } = useGoodsQueryForm();
   return (
     <PageContainer>
       <SearchTit>Category</SearchTit>
       <ResultItems
         isSearch={false}
-        keyword={params.categoryid}
         qs={goodsQueryString}
         onClickSort={onClickGoodsQS}
       />
--- src/pages/SearchResultPage.tsx
+++ src/pages/SearchResultPage.tsx
@@ -3,19 +3,14 @@ import SearchField from 'components/search/SearchField';
 import ResultItems from 'components/search/ResultItems';
 import React from 'react';
 import useGoodsQueryForm from 'hooks/search/useGoodsQueryForm';
-import { useSearchParams } from 'react-router-dom';
 
 function SearchResultPage() {
-  const [searchParams] = useSearchParams();
-  const { goodsQueryString, onClickGoodsQS } = useGoodsQueryForm(
-    searchParams.get('name'),
-  );
+  const { goodsQueryString, onClickGoodsQS } = useGoodsQueryForm();
   return (
     <PageContainer>
       <SearchField />
       <ResultItems
         isSearch
-        keyword={searchParams.get('name')}
         qs={goodsQueryString}
         onClickSort={onClickGoodsQS}
       />
