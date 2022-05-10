0b520754e97e0ceb9a2e64880b413304182ba2e4
이지호 <cctv1998@naver.com>
2022-05-10 19:27:05 +0900

    Add : Empty img

--- src/assets/img/index.ts
+++ src/assets/img/index.ts
@@ -1,3 +1,4 @@
 export { default as TextLogo } from './TextLogo.png';
 export { default as NotFoundImg } from './PageNotFound.png';
 export { default as Banner } from './Banner.png';
+export { default as Empty } from './Empty.png';
