57290398b81ba47c9a4e81ffad8069f3332da08c
이지호 <cctv1998@naver.com>
2022-05-13 22:44:04 +0900

    Feat : usePostGoodsForm

--- src/hooks/post/usePostGoodsForm.ts
+++ src/hooks/post/usePostGoodsForm.ts
@@ -1,16 +1,40 @@
 import useForm from 'hooks/common/useForm';
-import React from 'react';
+import useDetailUpload from 'hooks/imgs/useDetailUpload';
+import useThumbUpload from 'hooks/imgs/useThumbUpload';
+import { postGoodsAPI } from 'libs/api/goodsAPI';
+import { IRequestGoods } from 'model/goods';
 
 function usePostGoodsForm() {
-  const { form, onChangeForm, onResetForm } = useForm({
-    category: 'PANTS',
-    name: '수정된 데님',
-    price: '200000',
-    discount: '10',
-    stock: '1000',
-    detail: [],
-    thumbnail: '',
-  });
+  const { form, onChangeForm, onResetForm, onCustomChange } =
+    useForm<IRequestGoods>({
+      category: '',
+      name: '',
+      price: 0,
+      discount: 0,
+      detail: [],
+      thumbnail: '',
+    });
+  const { onThumbUpload } = useThumbUpload(onCustomChange);
+  const { onDetailUpload } = useDetailUpload(onCustomChange);
+
+  const onPostGoods = async () => {
+    try {
+      postGoodsAPI(form);
+    } catch (error) {
+      // eslint-disable-next-line no-alert
+      alert('공고 작성에 실패했습니다');
+      // eslint-disable-next-line no-console
+      console.log(error);
+    }
+  };
+  return {
+    form,
+    onChangeForm,
+    onResetForm,
+    onPostGoods,
+    onThumbUpload,
+    onDetailUpload,
+  };
 }
 
 export default usePostGoodsForm;
