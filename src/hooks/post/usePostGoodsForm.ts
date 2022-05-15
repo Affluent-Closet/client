8457511c61f25d31244d59b80290ef686c002a04
이지호 <cctv1998@naver.com>
2022-05-15 15:25:52 +0900

    Feat : onPostGoods

--- src/hooks/post/usePostGoodsForm.ts
+++ src/hooks/post/usePostGoodsForm.ts
@@ -3,8 +3,13 @@ import useDetailUpload from 'hooks/imgs/useDetailUpload';
 import useThumbUpload from 'hooks/imgs/useThumbUpload';
 import { postGoodsAPI } from 'libs/api/goodsAPI';
 import { IRequestGoods } from 'model/goods';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Path from 'routes/Path';
 
 function usePostGoodsForm() {
+  const navigate = useNavigate();
+  const [nexpPageId, setNexpPageId] = useState<number>(0);
   const { form, onChangeForm, onResetForm, onCustomChange } =
     useForm<IRequestGoods>({
       category: '',
@@ -19,13 +24,15 @@ function usePostGoodsForm() {
 
   const onPostGoods = async () => {
     try {
-      postGoodsAPI(form);
+      const response = await postGoodsAPI(form);
+      setNexpPageId(response.id);
     } catch (error) {
       // eslint-disable-next-line no-alert
       alert('공고 작성에 실패했습니다');
       // eslint-disable-next-line no-console
       console.log(error);
     }
+    if (nexpPageId !== 0) navigate(`${Path.PostGoodsPage}/${nexpPageId}`);
   };
   return {
     form,
