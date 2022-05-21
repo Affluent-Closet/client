15076989b51bae0c8153db038a75ebdf02ff1a58
이지호 <cctv1998@naver.com>
2022-05-21 22:38:20 +0900

    Chore: 베포 버그수정

--- src/libs/api/payAPI.ts
+++ src/libs/api/payAPI.ts
@@ -3,6 +3,5 @@ import client from '.';
 
 export const postPaymentAPI = async (body: QueryParams) => {
   const response = await client.post(`/pay/success`, body);
-  console.log(response);
   return response.data;
 };
