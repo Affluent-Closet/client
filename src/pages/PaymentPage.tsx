5a55eb2203787392be23966c91a70d263498e071
이지호 <cctv1998@naver.com>
2022-05-21 22:31:56 +0900

    결제 테스트

--- src/pages/PaymentPage.tsx
+++ src/pages/PaymentPage.tsx
@@ -16,7 +16,7 @@ function PaymentPage() {
     orderId: 'brL9yXo4bVrpAHe7p5sgZd2',
     orderName: '토스 티셔츠 외 2건!',
     customerName: '박토스',
-    successUrl: 'http://localhost:3000/payment',
+    successUrl: 'http://localhost:3000/success',
     failUrl: 'http://localhost:3000/false',
   });
 
