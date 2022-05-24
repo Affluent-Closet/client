bf48ff1f2a15069ad5e642ff67ee097cbbb41b24
이지호 <cctv1998@naver.com>
2022-05-25 00:15:31 +0900

    Feat: useReviewLoad

---
+++ src/hooks/review/useReviewLoad.ts
@@ -0,0 +1,20 @@
+import { getReviewAPI } from 'libs/api/reviewAPI';
+import { IReviewQuery, IReviewResponse } from 'model/review';
+import { useQuery } from 'react-query';
+
+function useReviewLoad(reviewQueryString: IReviewQuery) {
+  const { pageNo, sortBy, goodsId, pageSize } = reviewQueryString;
+  const reviewData = useQuery<IReviewResponse>(
+    ['review', pageNo, sortBy, goodsId, pageSize],
+    () => getReviewAPI(reviewQueryString),
+    {
+      cacheTime: 5 * 60 * 1000,
+      staleTime: 5 * 60 * 1000,
+    },
+  );
+  // eslint-disable-next-line no-console
+  console.log(reviewQueryString, reviewData.data);
+  return { reviewData };
+}
+
+export default useReviewLoad;
