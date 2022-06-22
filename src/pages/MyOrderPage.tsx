c9a96db6e22ece95979cb7a8655b11bd319a5747
이지호 <cctv1998@naver.com>
2022-06-22 22:46:47 +0900

    Feat: MyorderPage Header

--- src/pages/MyOrderPage.tsx
+++ src/pages/MyOrderPage.tsx
@@ -1,7 +1,22 @@
+import { ListHead, PageLayout } from 'components/common/CommonComponents';
+import OrderSummery from 'components/myOrder/OrderSummery';
+import OrderInfo from 'components/order/OrderInfo';
+import OrderShippingInfo from 'components/order/OrderShippingInfo';
 import React from 'react';
+import styled from 'styled-components';
+
+const OrderListHead = styled(ListHead)`
+  border: none;
+`;
 
 function MyOrderPage() {
-  return <div>MyOrder</div>;
+  return (
+    <PageLayout>
+      <OrderListHead>주문내역 조회</OrderListHead>
+      <OrderSummery />
+      <OrderInfo />
+    </PageLayout>
+  );
 }
 
 export default MyOrderPage;
