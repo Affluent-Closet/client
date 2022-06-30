608662db1cb610faf8c29439a14fc580f09b435f
이지호 <cctv1998@naver.com>
2022-06-30 20:44:56 +0900

    Test: onResetForm 함수를 실행했을 때 value 값이 초기화된다.

--- src/hooks/common/_test_/useForm.test.ts
+++ src/hooks/common/_test_/useForm.test.ts
@@ -1,4 +1,5 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { act, renderHook } from '@testing-library/react-hooks';
+import React from 'react';
 import useForm from '../useForm';
 
 const MOCK_FORM_DATA = {
@@ -7,10 +8,31 @@ const MOCK_FORM_DATA = {
 };
 
 describe('useForm', () => {
-  it('useForm은 요소가 3개인 객체를 리턴한다. (form,onChangeForm,onResetForm)', () => {
+  test('useForm은 요소가 3개인 객체를 리턴한다. {form,onChangeForm,onResetForm}', () => {
     const { result } = renderHook(() => {
       useForm(MOCK_FORM_DATA);
     });
     expect(Object.keys(result)).toHaveLength(3);
+  });
+
+  test('onResetForm 함수를 실행했을 때 value 값이 초기화된다.', () => {
+    const { result } = renderHook(() => useForm(MOCK_FORM_DATA));
+
+    act(() => {
+      result.current.onChangeForm({
+        target: {
+          name: 'text',
+          value: '16',
+        },
+      } as React.ChangeEvent<HTMLInputElement>);
+    });
+
+    expect(result.current.form.text).toBe('16');
+
+    act(() => {
+      result.current.onResetForm();
+    });
+
+    expect(result.current.form).toBe(MOCK_FORM_DATA);
   });
 });
