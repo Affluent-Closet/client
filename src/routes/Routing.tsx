b2d5e629d55a89280990f49adcc60d3f6b47b017
이지호 <cctv1998@naver.com>
2022-07-08 14:55:35 +0900

    Feat : LazyLoad 적용

--- src/routes/Routing.tsx
+++ src/routes/Routing.tsx
@@ -1,50 +1,53 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import LandingPage from 'pages/LandingPage';
-import GoodsPage from 'pages/GoodsPage';
-import SearchPage from 'pages/SearchPage';
-import SearchResultPage from 'pages/SearchResultPage';
-import OrderPage from 'pages/OrderPage';
-import CategoryPage from 'pages/CategoryPage';
-import NotFoundPage from 'pages/NotFoundPage';
-import LoginPage from 'pages/LoginPage';
-import RegisterPage from 'pages/RegisterPage';
-import ProfilePage from 'pages/ProfilePage';
-import CartPage from 'pages/CartPage';
-import PostGoodsPage from 'pages/PostGoodsPage';
-import PostReviewPage from 'pages/PostReviewPage';
-import LogoutPage from 'pages/LogoutPage';
-import PostSizePage from 'pages/PostSizePage';
-import PaymentPage from 'pages/PaymentPage';
-import SuccessPage from 'pages/SuccessPage';
-import NewBestPage from 'pages/NewBestPage';
-import MyOrderPage from 'pages/MyOrderPage';
 import Path from './Path';
+
+const LandingPage = lazy(() => import('pages/LandingPage'));
+const GoodsPage = lazy(() => import('pages/GoodsPage'));
+const SearchPage = lazy(() => import('pages/SearchPage'));
+const SearchResultPage = lazy(() => import('pages/SearchResultPage'));
+const OrderPage = lazy(() => import('pages/OrderPage'));
+const CategoryPage = lazy(() => import('pages/CategoryPage'));
+const LoginPage = lazy(() => import('pages/LoginPage'));
+const RegisterPage = lazy(() => import('pages/RegisterPage'));
+const ProfilePage = lazy(() => import('pages/ProfilePage'));
+const CartPage = lazy(() => import('pages/CartPage'));
+const PostGoodsPage = lazy(() => import('pages/PostGoodsPage'));
+const PostReviewPage = lazy(() => import('pages/PostReviewPage'));
+const LogoutPage = lazy(() => import('pages/LogoutPage'));
+const PostSizePage = lazy(() => import('pages/PostSizePage'));
+const PaymentPage = lazy(() => import('pages/PaymentPage'));
+const SuccessPage = lazy(() => import('pages/SuccessPage'));
+const NewBestPage = lazy(() => import('pages/NewBestPage'));
+const MyOrderPage = lazy(() => import('pages/MyOrderPage'));
+const NotFoundPage = lazy(() => import('pages/NotFoundPage'));
 
 function Routing() {
   return (
-    <Routes>
-      <Route path="*" element={<NotFoundPage />} />
-      <Route path={Path.LandingPage} element={<LandingPage />} />
-      <Route path={Path.GoodsPage} element={<GoodsPage />} />
-      <Route path={Path.SearchPage} element={<SearchPage />} />
-      <Route path={Path.SearchResultPage} element={<SearchResultPage />} />
-      <Route path={Path.OrderPage} element={<OrderPage />} />
-      <Route path={Path.CategoryPage} element={<CategoryPage />} />
-      <Route path={Path.LoginPage} element={<LoginPage />} />
-      <Route path={Path.RegisterPage} element={<RegisterPage />} />
-      <Route path={Path.ProfilePage} element={<ProfilePage />} />
-      <Route path={Path.CartPage} element={<CartPage />} />
-      <Route path={Path.PostGoodsPage} element={<PostGoodsPage />} />
-      <Route path={Path.PostReviewPage} element={<PostReviewPage />} />
-      <Route path={Path.LogoutPage} element={<LogoutPage />} />
-      <Route path={Path.PostGoodsSizePage} element={<PostSizePage />} />
-      <Route path={Path.PaymentPage} element={<PaymentPage />} />
-      <Route path={Path.SuccessPage} element={<SuccessPage />} />
-      <Route path={Path.NewPage} element={<NewBestPage />} />
-      <Route path={Path.BestPage} element={<NewBestPage />} />
-      <Route path={Path.MyOrderPage} element={<MyOrderPage />} />
-    </Routes>
+    <Suspense fallback={<div style={{ minHeight: '100vh' }} />}>
+      <Routes>
+        <Route path="*" element={<NotFoundPage />} />
+        <Route path={Path.LandingPage} element={<LandingPage />} />
+        <Route path={Path.GoodsPage} element={<GoodsPage />} />
+        <Route path={Path.SearchPage} element={<SearchPage />} />
+        <Route path={Path.SearchResultPage} element={<SearchResultPage />} />
+        <Route path={Path.OrderPage} element={<OrderPage />} />
+        <Route path={Path.CategoryPage} element={<CategoryPage />} />
+        <Route path={Path.LoginPage} element={<LoginPage />} />
+        <Route path={Path.RegisterPage} element={<RegisterPage />} />
+        <Route path={Path.ProfilePage} element={<ProfilePage />} />
+        <Route path={Path.CartPage} element={<CartPage />} />
+        <Route path={Path.PostGoodsPage} element={<PostGoodsPage />} />
+        <Route path={Path.PostReviewPage} element={<PostReviewPage />} />
+        <Route path={Path.LogoutPage} element={<LogoutPage />} />
+        <Route path={Path.PostGoodsSizePage} element={<PostSizePage />} />
+        <Route path={Path.PaymentPage} element={<PaymentPage />} />
+        <Route path={Path.SuccessPage} element={<SuccessPage />} />
+        <Route path={Path.NewPage} element={<NewBestPage />} />
+        <Route path={Path.BestPage} element={<NewBestPage />} />
+        <Route path={Path.MyOrderPage} element={<MyOrderPage />} />
+      </Routes>
+    </Suspense>
   );
 }
 
